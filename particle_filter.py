dda4334366161353330945b44abfbf90fa8af639
Ysuyuki Fujii <is0232xf@gmail.com>
2016-09-08 11:22:51 +0900

    サンプリングする関数(sampling())を追加した

--- particle_filter.py
+++ particle_filter.py
@@ -5,6 +5,37 @@ Created on Tue Sep 06 12:28:02 2016
 @author: Fujiichang
 """
 
+import bayes_filter
+
+
+p_o_s = [[0.01, 0.01, 0.01, 0.01, 0.85, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01],
+         [0.01, 0.01, 0.01, 0.01, 0.01, 0.85, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01],
+         [0.01, 0.01, 0.01, 0.01, 0.01, 0.85, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01],
+         [0.01, 0.01, 0.01, 0.01, 0.01, 0.85, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01],
+         [0.01, 0.85, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]]
+
+p_s_a = [[[1, 0, 0, 0, 0], [0.9, 0.1, 0, 0, 0], [0, 0.9, 0.1, 0, 0], [0, 0, 0.9, 0.1, 0], [0, 0, 0, 0.9, 0.1]],
+         [[0.1, 0.9, 0, 0, 0], [0, 0.1, 0.9, 0, 0], [0, 0, 0.1, 0.9, 0], [0, 0, 0, 0.1, 0.9],  [0, 0, 0, 0, 1]],
+         [[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]]
+
+
+class ParticleFilter(object):
+    def __init__(self):
+        self.p_s_a = p_s_a
+        self.p_o_s = p_o_s
+
+    def sampling(self, a, particle):
+        for i in range(5):
+            s = particle[i][len(particle[i]) - 1]
+            p_s = bayes_filter.p_s_a[a][s]
+            new_s = bayes_filter.multinomial(p_s)
+            particle[i].append(new_s)
+            print particle
+
+
 if __name__ == "__main__":
 
-
+    simulater = ParticleFilter()
+    particle_num = 5
+    particle = [[0 for i in range(1)] for j in range(particle_num)]
+    simulater.sampling(1, particle)
